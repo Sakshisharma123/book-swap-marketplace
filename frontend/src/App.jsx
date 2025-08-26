1a877b8a72762e2800637d358aa0c6b3d4363278
Sakshishrma123 <sakshisharma.dsvv@gmail.com>
2025-08-26 12:25:32 +0530

    create stucture of project for  frontend and backend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+
+  return (
+    <>
+      <div className="min-h-scrren bg-amber-200 flex justify-center mx-auto">
+        <h1>Book Swap Marketplace</h1>
+      </div>
+
+    </>
+  )
+}
+
+export default App
